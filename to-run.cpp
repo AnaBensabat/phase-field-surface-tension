128f642f95b39b4477a35cb9f87af129a1089a60
Ana Bensabat <anabpaulino@gmail.com>
2024-06-10 11:13:37 +0100

    update scale and eps

--- to-run.cpp
+++ to-run.cpp
@@ -15,7 +15,7 @@ void run_simulation(string pwd, double eta, double kappa){
   
   // 5 microns for the grooves -> 10 points
   // cell radius of 25 -> 50 points
-  // nucleus radius of 10 -> 20 points >.<
+  // nucleus radius of 10 -> 20 points 
   
   //MAKE GROOVES
   //geometry parameters
@@ -39,7 +39,7 @@ void run_simulation(string pwd, double eta, double kappa){
   //MAKE CELL
   //geometry parameters
   vector<double> radius = {28*scale,15*scale};
-  vector<double> radius_nucleus = {11*scale, 7*scale};
+  vector<double> radius_nucleus = {15*scale, 7*scale};
   depth = width;
   vector<double> center_coordinates = {(double)dimX/2,(double)dimY/2,(double)depth*2};
   vector<double> nucleus_coordinates = {(double)dimX/2-2,(double)dimY/2-2,(double)depth*2+1};
@@ -56,8 +56,8 @@ void run_simulation(string pwd, double eta, double kappa){
   mycell.alpha = 0.001/12;      //volume coefficient
   mycell.gamma = 1;             //repulsion term
   mycell.eta = eta;             //adhesion coefficient 
-  mycell.kappa = kappa;             //surface tension fraction // este é o rácio
-  mycell.chi = 100;           //non local term
+  mycell.kappa = kappa;         //surface tension fraction // este é o rácio
+  mycell.chi = 100;             //non local term
   double velocity = 0.;         //drag velocity
   double velocity_nuc = 0;
   
@@ -66,16 +66,15 @@ void run_simulation(string pwd, double eta, double kappa){
   evolve(mycell, mygrooves.grid, 0, 0, dt, time, pwd);
   cout<<"----CELL STABILIZATION----"<<endl;
   
-  string s1 = "cell_0_eta="+to_string(mycell.eta)+"_k="+to_string(mycell.kappa)+".vti";
-  string s2 = "nucleus_0_eta="+to_string(mycell.eta)+"_k="+to_string(mycell.kappa)+".vti";
-  string s3 = "environment_0_eta="+to_string(mycell.eta)+"_k="+to_string(mycell.kappa)+".vti";
+  string s1 = "cell_0_eta="+to_string(mycell.eta)+"_k="+to_string(mycell.kappa)+"_eps="+to_string(mycell.epsilon)+".vti";
+  string s2 = "nucleus_0_eta="+to_string(mycell.eta)+"_k="+to_string(mycell.kappa)+"_eps="+to_string(mycell.epsilon)+".vti";
+  string s3 = "environment_0_eta="+to_string(mycell.eta)+"_k="+to_string(mycell.kappa)+"_eps="+to_string(mycell.epsilon)+".vti";
   saveGridToVTI(pwd+s1,mycell.grid);
   saveGridToVTI(pwd+s2,mycell.grid_nucleus);
   saveGridToVTI(pwd+s3,mygrooves.grid);
   
   double v0_cell = vol(mycell.grid,1e-6);
   double v0_nucleus = vol(mycell.grid_nucleus,1e-6);
-  //cout<<v0_cell<<'\n';
   
   double blows = 0;
   if (v0_cell<1e-3) {
@@ -83,14 +82,14 @@ void run_simulation(string pwd, double eta, double kappa){
     cout<<"----BLOWS UP----"<<endl;
   }
   else{
-    time = 5;
-    //cout<<"before "<<vol(mycell.grid,1e-6)<<'\n';
+    time = 2000;
+
     cout<<"----STARTING TIME EVOLUTION----"<<endl;
-    evolve(mycell, mygrooves.grid, velocity, velocity_nuc,  dt, time, pwd, 2);
+    evolve(mycell, mygrooves.grid, velocity, velocity_nuc,  dt, time, pwd, 500);
     
-    s1 = "cell_F_eta="+to_string(mycell.eta)+"_k="+to_string(mycell.kappa)+".vti";
-    s2 = "nucleus_F_eta="+to_string(mycell.eta)+"_k="+to_string(mycell.kappa)+".vti";
-    s3 = "environment_F_eta="+to_string(mycell.eta)+"_k="+to_string(mycell.kappa)+".vti";
+    s1 = "cell_F_eta="+to_string(mycell.eta)+"_k="+to_string(mycell.kappa)+"_eps="+to_string(mycell.epsilon)+".vti";
+    s2 = "nucleus_F_eta="+to_string(mycell.eta)+"_k="+to_string(mycell.kappa)+"_eps="+to_string(mycell.epsilon)+".vti";
+    s3 = "environment_F_eta="+to_string(mycell.eta)+"_k="+to_string(mycell.kappa)+"_eps="+to_string(mycell.epsilon)+".vti";
     saveGridToVTI(pwd+s1,mycell.grid);
     saveGridToVTI(pwd+s2,mycell.grid_nucleus);
     saveGridToVTI(pwd+s3,mygrooves.grid);
@@ -111,10 +110,8 @@ int main(){
   filesystem::create_directory("output");
   pwd+="/output/";
   
-  run_simulation(pwd);
-
-  vector<double> etas = {0.7};//{0.1, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3, 1.5, 1.7, 1.9};
-  vector<double> kappas = {0.1};//{0.1, 0.5, 1, 1.5, 2, 2.5, 3};
+  vector<double> etas = {0.1, 0.5, 1, 1.5};//{0.1, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3, 1.5, 1.7, 1.9};
+  vector<double> kappas = {0.1, 0.5, 1, 15, 2};//{0.1, 0.5, 1, 1.5, 2, 2.5, 3};
 
   for(auto eta:etas){
     for(auto kappa:kappas){
